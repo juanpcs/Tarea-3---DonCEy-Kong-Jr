c3345be737901bfae155481edddc0433130767ba
Erick <efmz2000@outlook.es>
2021-04-27 16:18:55 -0600

    ver jugador en el espectador

--- sdlDKJ/espectador.c
+++ sdlDKJ/espectador.c
@@ -3,9 +3,89 @@
 #include <stdbool.h>
 #include "mono.c"
 #include<winsock2.h>
+#include <stdio.h>
 
+int charToInt(char *a, int largo){
+    int num =0;
+    int in = 1;
+    for(int i=largo-1; i>=0; i--){
+        num += (a[i]-'0')*in;
+        in = in *10;
+    }
+    return num;
+}
+int getLargo(char *cadena){
+    int i = 0;
+    while(*cadena != '\0'){
+        i ++;
+        cadena ++;
+    }
+    return i;
+}
+void posmono(Junior *mon,SOCKET s){
+    char* response[2000];
+    char mensaje[]= "getJugador1\n";
+    enviar(s,mensaje,response);
+    printf("%c\n", &response);
 
-void espectador1(int vidas,SOCKET s){
+    char *tocken = strtok(response,";");
+    int x=charToInt(tocken,getLargo(tocken));
+    mon->x=x;
+    tocken= strtok(NULL,";");
+    int y=charToInt(tocken,getLargo(tocken));
+    mon->y=y;
+}
+
+int eventosEspectador1(SDL_Window *window)
+{
+
+  SDL_Event event;
+  int done = 0;
+
+  //Función que esta atenta a los eventos principamente usada para revisar si se cierra la ventana
+  //o si
+  while(SDL_PollEvent(&event))
+  {
+    switch(event.type)
+      {
+          case SDL_WINDOWEVENT_CLOSE:
+              {
+              if(window)
+                {
+                  SDL_DestroyWindow(window);
+                  window = NULL;
+                  done = 1;
+                }
+              }
+        break;
+          case SDL_KEYDOWN:
+              {
+                switch(event.key.keysym.sym)
+                {
+                  case SDLK_ESCAPE:
+                    done = 1;
+                  break;
+                }
+              }
+        break;
+        case SDL_QUIT:
+            {
+                //quit out of the game
+                done = 1;
+                break;
+            }
+        }
+    }
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+
+    if(state[SDL_SCANCODE_KP_1]){
+        done = 1;
+    }
+
+    return done;
+}
+
+void espectador1(SOCKET s){
 
     //Inicio de SDL y carga de ventana
     SDL_Event event;
@@ -25,7 +105,7 @@ void espectador1(int vidas,SOCKET s){
     //int xpos[]={180,375,510,565,622,750,790,980,1030};
 
     Junior mono;
-
+    posmono(&mono,s);
 
     //Croco rcroc;
     //rcroc.direccion=1;
@@ -47,6 +127,7 @@ void espectador1(int vidas,SOCKET s){
 
     while (!done)
     {
+        done = eventosEspectador1(window);
         //if(vida==0){
         //    done=1;
         //    }
@@ -62,12 +143,15 @@ void espectador1(int vidas,SOCKET s){
         //moverCroc(&rcroc);
         //gravedad(&mono,s);
         //colidCoc(&mono,&rcroc,s);
-        //SDL_RenderCopy(renderer,texture,NULL,NULL);
-        //done=processEvents(window,&mono,s);
+
+        //Actualiza la posción del mono
+        posmono(&mono,s);
+        SDL_RenderCopy(renderer,texture,NULL,NULL);
+        done=processEvents(window,&mono,s);
 
         //dibujar el mono
-        //SDL_Rect rect = { mono.x, mono.y, 40, 50 };
-        //SDL_RenderCopyEx(renderer, mono.sheetTexture, NULL, &rect, 0, NULL, 0);
+        SDL_Rect rect = { mono.x, mono.y, 40, 50 };
+        SDL_RenderCopyEx(renderer, mono.sheetTexture, NULL, &rect, 0, NULL, 0);
 
         //dibuja el cocodrilo
         //SDL_Rect rect2 = { rcroc.x, rcroc.y, 21, 40 };
@@ -75,9 +159,10 @@ void espectador1(int vidas,SOCKET s){
 
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(50000);
-        done=1;
+        SDL_Delay(100);
+
     }
+
     char* response[2000];
     char mensaje[]= "DescEsp1\n";
     enviar(s,mensaje,response);
